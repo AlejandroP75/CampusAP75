9989f76efb9b7347c01de0d096f3f70da9dbd261
AlejandroP75 <69017469+AlejandroP75@users.noreply.github.com>
2023-09-30 18:11:01 -0500

    Actualizado

--- JavaScript/Ejercicios/ejercicio1.js
+++ JavaScript/Ejercicios/ejercicio1.js
@@ -14,44 +14,44 @@ let c3 = document.querySelector("#c3");
 function cartas(num, car){
     switch (num){
         case 1:
-            car.src = "1.webp";
+            car.src = "imagenes ejercicio 1/1.webp";
             break;
         case 2:
-            car.src = "2.png";
+            car.src = "imagenes ejercicio 1/2.png";
             break;
         case 3:
-            car.src = "3.png";
+            car.src = "imagenes ejercicio 1/3.png";
             break;
         case 4:
-            car.src = "4.png";
+            car.src = "imagenes ejercicio 1/4.png";
             break;
         case 5:
-            car.src = "5.png";
+            car.src = "imagenes ejercicio 1/5.png";
             break;
         case 6:
-            car.src = "6.png";
+            car.src = "imagenes ejercicio 1/6.png";
             break;
         case 7:
-            car.src = "7.png";
+            car.src = "imagenes ejercicio 1/7.png";
             break;
         case 8:
-            car.src = "8.png";
+            car.src = "imagenes ejercicio 1/8.png";
             break;
         case 9:
-            car.src = "9.png";
+            car.src = "imagenes ejercicio 1/9.png";
             break;
         case 10:
-            car.src = "10.png";
+            car.src = "imagenes ejercicio 1/10.png";
             break;
         case 11:
-            car.src = "11.webp";
+            car.src = "imagenes ejercicio 1/11.webp";
             break;
     }
 }
 
 
 function generarNum1(){
-    c2.src = "carta_trasera.webp";
+    c2.src = "imagenes ejercicio 1/carta_trasera.webp";
     n1 = Math.floor(Math.random() * 11) + 1;
     num1.textContent = n1;
     cartas(n1, c1);
@@ -59,14 +59,14 @@ function generarNum1(){
         let n3 = n1 + n2;
         fibbo.forEach((element) => {
             if(element == n3){
-                c2.src = "fibo.avif";
+                c2.src = "imagenes ejercicio 1/fibo.avif";
             }
         })
     }
 }
 
 function generarNum2(){
-    c2.src = "carta_trasera.webp";
+    c2.src = "imagenes ejercicio 1/carta_trasera.webp";
     n2 = Math.floor(Math.random() * 11) + 1;
     num2.textContent = n2;
     cartas(n2, c3);
@@ -74,7 +74,7 @@ function generarNum2(){
         let n3 = n1 + n2;
         fibbo.forEach((element) => {
             if(element == n3){
-                c2.src = "fibo.avif";
+                c2.src = "imagenes ejercicio 1/fibo.avif";
             }
         })
     }
