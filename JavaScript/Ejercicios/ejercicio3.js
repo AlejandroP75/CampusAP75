2dfbb896fd29b2018b8587ad154ae97a31c03221
AlejandroP75 <69017469+AlejandroP75@users.noreply.github.com>
2023-10-01 20:26:42 -0500

    Actualizado

--- JavaScript/Ejercicios/ejercicio3.js
+++ JavaScript/Ejercicios/ejercicio3.js
@@ -1,37 +1,57 @@
 let d = document;
-const contenedor = d.querySelector(".contenedor");
-const botonAñadirTarea = d.querySelector("#botAñadir");
+let contenedor = d.querySelector(".contenedor");
+let botonAñadirTarea = d.querySelector("#botAñadir");
 let cajaTexto = d.querySelector("#tarea");
-let checko = 0;
 
 function añadirTarea() {
     let tarea = cajaTexto.value;
     if (tarea == "") {
-        alert("No puedes agregar una tarea vacia");
+        alert("No puedes agregar una tarea vacía");
         return;
     }
     let miniCaja = d.createElement("div");
-    miniCaja.setAttribute("style", "display: flex;");
-
+    miniCaja.setAttribute("class", "tareitas");
+    let miniminiCaja = d.createElement("div");
+    miniminiCaja.setAttribute("class", "miniTareas");
 
     let check = d.createElement("input");
-    check.setAttribute("type" ,"checkbox");
-    checko = check;
+    check.setAttribute("type", "checkbox");
+    check.addEventListener("change", function () {
+        marcar(this);
+    });
     let label = d.createElement("label");
-    label.textContent = tarea
-    const br = d.createElement("br");
+    label.textContent = tarea;
+    let br = d.createElement("br");
     let eliminar = d.createElement("button");
+    eliminar.setAttribute("class", "botonEliminar");
     eliminar.textContent = "Eliminar";
+    eliminar.addEventListener("click", function () {
+        eliminarTarea(miniCaja);
+    });
 
     contenedor.appendChild(miniCaja);
-    miniCaja.appendChild(check);
-    miniCaja.appendChild(label);
+    miniCaja.appendChild(miniminiCaja);
+    miniminiCaja.appendChild(check);
+    miniminiCaja.appendChild(label);
     miniCaja.appendChild(br);
     miniCaja.appendChild(eliminar);
+
+    cajaTexto.value = "";
 }
 
-function marcar() {
-    console.log("This is a test");
+function marcar(checkbox) {
+    let labeld = checkbox.nextElementSibling;
+    if (checkbox.checked) {
+        labeld.style.textDecoration = "line-through";
+        labeld.style.color = "#000706";
+    } else {
+        labeld.style.textDecoration = "none";
+        labeld.style.color = "white";
+    }
+}
+
+function eliminarTarea(taskContainer) {
+    contenedor.removeChild(taskContainer);
 }
 
 botonAñadirTarea.addEventListener("click", añadirTarea);
