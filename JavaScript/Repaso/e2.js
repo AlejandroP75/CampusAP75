390841ca76aebbbb1642bc680a85bb076a08d33d
AlejandroP75 <diego_palacio5@hotmail.com>
2023-10-03 14:47:35 -0500

    Actualizado

--- JavaScript/Repaso/e2.js
+++ JavaScript/Repaso/e2.js
@@ -18,10 +18,31 @@ function calcular(){
     }else{
         let x1 = ((-b+Math.sqrt((b*b)-4*a*c)) / (2*a));
         let x2 = ((-b-Math.sqrt((b*b)-4*a*c)) / (2*a));
-        x1res.textContent = "X1 = " + x1;
-        x2res.textContent = "X2 = " + x2;
+        x1res.textContent = "x1 = " + x1;
+        x2res.textContent = "x2 = " + x2;
     }
 }
 
 boton.addEventListener("click", calcular);
 
+const estiloSelector = document.getElementById('estiloSelector');
+const guardarEstilo = document.getElementById('guardarEstilo');
+const estilos = document.getElementById('estilos');
+
+guardarEstilo.addEventListener('click', function () {
+    const estiloSeleccionado = estiloSelector.value;
+
+    if (estiloSeleccionado) {
+        localStorage.setItem('estilo', estiloSeleccionado);
+        estilos.setAttribute('href', estiloSeleccionado + '.css');
+    }
+});
+
+window.addEventListener('load', function () {
+    const estiloAlmacenado = localStorage.getItem('estilo');
+
+    if (estiloAlmacenado) {
+        estilos.setAttribute('href', estiloAlmacenado + '.css');
+        estiloSelector.value = estiloAlmacenado;
+    }
+});
