22cd008a65bb3dc79de52d4e5150878dd162b2cc
AlejandroP75 <69017469+AlejandroP75@users.noreply.github.com>
2023-10-07 22:58:43 -0500

    Entrega commit

--- JavaScript/Ejercicios/ejercicio5.js
+++ JavaScript/Ejercicios/ejercicio5.js
@@ -2,10 +2,8 @@ let portLib = document.getElementById("linkLib");
 let titLib = document.getElementById("titLib");
 let autLib = document.getElementById("autLib");
 let anioLib = document.getElementById("anioLib");
-let disLib = document.getElementById("disLib");
-let presLib = document.getElementById("presLib");
 let botAniadir = document.getElementById("aniaLib");
-let form = document.getElementById("nuevosLibros");
+let form = document.getElementById("biblio");
 
 function añadirLibro(){
     console.log("This is a test");
@@ -13,16 +11,15 @@ function añadirLibro(){
     let titulo = titLib.value;
     let autor = autLib.value;
     let anio = anioLib.value;
-    let disponible = disLib.value;
-    let prestado = presLib.value;
 
     let contenedor = document.createElement("div");
+    contenedor.setAttribute("class", "libro");
     let portadaE = document.createElement("img");
     portadaE.setAttribute("src", portada);
     portadaE.setAttribute("width", "200px");
     portadaE.setAttribute("height", "200px");
     let textE = document.createElement("p");
-    textE.innerHTML = "<br>Titulo: " + titulo + "<br>Autor: " + autor + "<br>Año: " + anio +"<br>Disponible: " + disponible +"<br>Prestado: " + prestado;
+    textE.innerHTML = "<br>Titulo: " + titulo + "<br>Autor: " + autor + "<br>Año: " + anio;
   
     form.appendChild(contenedor);
     contenedor.appendChild(portadaE);
