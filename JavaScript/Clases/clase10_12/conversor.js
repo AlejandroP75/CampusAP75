35b4e75c6483737a6e082cd100a67b6c02f910f7
AlejandroP75 <diego_palacio5@hotmail.com>
2023-10-13 15:43:12 -0500

    Primer commit

--- JavaScript/Clases/clase10_12/conversor.js
+++ JavaScript/Clases/clase10_12/conversor.js
@@ -24,6 +24,10 @@ function peso(){
     let dePv = deP.value;
     let aPv = aP.value;
 
+    if(ingresoP.value == ""){
+        alert("Ingrese un numero valido")
+    }
+
     if(dePv == "kg"){
         if(aPv == "lb"){
             let res = parseFloat(ingresoP.value)*2.205;
@@ -42,84 +46,215 @@ function peso(){
             resultadoP.textContent = res3;
         }
         else{
-            alert("COMO HPTAS ")
+            alert("Error! Vuelve a intentarlo !!!")
         }
     }
     if(dePv == "lb"){
         if(aPv == "kg"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)/2.205;
+            resultadoP.textContent = res;
         }
          else if(aPv == "arroba"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*25;
+            resultadoP.textContent = res;
         }
          else if(aPv == "gramos"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*453.6;
+            resultadoP.textContent = res;
         }
          else if(aPv == "onzas"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*28.35;
+            resultadoP.textContent = res;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
         }
     }
-        if(dePv == "lb"){
-        if(aPv == "kg"){
-            console.log("This is a test");
-        }
-        else if(aPv == "arroba"){
-            console.log("This is a test");
-        }
-        else if(aPv == "gramos"){
-            console.log("This is a test");
-        }
-        else if(aPv == "onzas"){
-            console.log("This is a test");
-        }
-    }
+
     if(dePv == "arroba"){
         if(aPv == "kg"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*12.5;
+            resultadoP.textContent = res;
         }
         else if(aPv == "lb"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*25;
+            resultadoP.textContent = res;
         }
         else if(aPv == "gramos"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*11339.81;
+            resultadoP.textContent = res;
         }
         else if(aPv == "onzas"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*405.72;
+            resultadoP.textContent = res;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
         }
     }
     if(dePv == "gramos"){
         if(aPv == "kg"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)/1000;
+            resultadoP.textContent = res;
         }
         else if(aPv == "lb"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)/453.6;
+            resultadoP.textContent = res;
         }
         else if(aPv == "arroba"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)/11339.809;
+            resultadoP.textContent = res;
         }
-        else {
-            console.log("This is a test");
+        else if(aPv == "arroba"){
+            let res = parseFloat(ingresoP.value)/28.35;
+            resultadoP.textContent = res;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
         }
     }
     if(dePv == "onzas"){
         if(aPv == "kg"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)/35.274;
+            resultadoP.textContent = res;
         }
         else if(aPv == "lb"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)/16;
+            resultadoP.textContent = res;
         }
         else if(aPv == "arroba"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*0.028349523125;
+            resultadoP.textContent = res;
         }
         else if(aPv == "gramos"){
-            console.log("This is a test");
+            let res = parseFloat(ingresoP.value)*28.35;
+            resultadoP.textContent = res;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
         }
     }
+
+}
+
+function longitud(){
+    let deLv = deL.value;
+    let aLv = aL.value;
+
+    if(ingresoL.value == ""){
+        alert("Ingrese un numero valido")
+    }
+
+    if(deLv == "m"){
+        if(aLv == "km"){
+            let res = parseFloat(ingresoL.value)/1000;
+            resultadoL.textContent = res;
+        }
+        else if(aLv == "milla"){
+            let res1 = parseFloat(ingresoL.value)/1609;
+            resultadoL.textContent = res1;
+        }
+        else if(aLv == "yarda"){
+            let res2 = parseFloat(ingresoL.value)*1094;
+            resultadoL.textContent = res2;
+        }
+         else if(aLv == "codo"){
+            let res3 = parseFloat(ingresoL.value)*2.25;
+            resultadoL.textContent = res3;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
+        }
+    }
+    if(deLv == "km"){
+        if(aLv == "m"){
+            let res = parseFloat(ingresoL.value)*1000;
+            resultadoL.textContent = res;
+        }
+        else if(aLv == "milla"){
+            let res = parseFloat(ingresoL.value)/1609;
+            resultadoL.textContent = res;
+        }
+        else if(aLv == "yarda"){
+            let res2 = parseFloat(ingresoL.value)/1094;
+            resultadoL.textContent = res2;
+        }
+         else if(aLv == "codo"){
+            let res3 = parseFloat(ingresoL.value)/2.25;
+            resultadoL.textContent = res3;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
+        }
+    }
+
+    if(deLv == "m"){
+        if(aLv == "km"){
+            let res = parseFloat(ingresoP.value)/1000;
+            resultadoP.textContent = res;
+        }
+        else if(aPv == "milla"){
+            let res1 = parseFloat(ingresoP.value)*0.088;
+            resultadoP.textContent = res1;
+        }
+        else if(aPv == "yarda"){
+            let res2 = parseFloat(ingresoP.value)*1000;
+            resultadoP.textContent = res2;
+        }
+         else if(aPv == "codo"){
+            let res3 = parseFloat(ingresoP.value)*35.;
+            resultadoP.textContent = res3;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
+        }
+    }
+    if(deLv == "m"){
+        if(aLv == "km"){
+            let res = parseFloat(ingresoP.value)/1000;
+            resultadoP.textContent = res;
+        }
+        else if(aPv == "milla"){
+            let res1 = parseFloat(ingresoP.value)*0.088;
+            resultadoP.textContent = res1;
+        }
+        else if(aPv == "yarda"){
+            let res2 = parseFloat(ingresoP.value)*1000;
+            resultadoP.textContent = res2;
+        }
+         else if(aPv == "codo"){
+            let res3 = parseFloat(ingresoP.value)*35.;
+            resultadoP.textContent = res3;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
+        }
+    }
+    if(dePv == "onzas"){
+        if(aPv == "kg"){
+            let res = parseFloat(ingresoP.value)/35.274;
+            resultadoP.textContent = res;
+        }
+        else if(aPv == "lb"){
+            let res = parseFloat(ingresoP.value)/16;
+            resultadoP.textContent = res;
+        }
+        else if(aPv == "arroba"){
+            let res = parseFloat(ingresoP.value)*0.028349523125;
+            resultadoP.textContent = res;
+        }
+        else if(aPv == "gramos"){
+            let res = parseFloat(ingresoP.value)*28.35;
+            resultadoP.textContent = res;
+        }
+        else{
+            alert("Error! Vuelve a intentarlo !!!")
+        }
+    }
+
 }
 
 
 
-
-
-
+convertirL.addEventListener("click",longitud);
 convertirP.addEventListener("click",peso);
