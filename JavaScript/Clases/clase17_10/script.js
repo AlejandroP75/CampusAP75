8b5081ad876deb46d0597132052616319fed6b44
AlejandroP75 <diego_palacio5@hotmail.com>
2023-10-18 13:27:53 -0500

    API Commit

---
+++ JavaScript/Clases/clase17_10/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const launchList = document.getElementById('launch-list');
+    const sortSelect = document.getElementById('sort');
+    const sortButton = document.getElementById('sort-button');
+
+    const apiUrl = 'https://api.spacexdata.com/v4/launches';
+
+    function fetchDataAndDisplay(sortBy) {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                data.sort((a, b) => {
+                    if (sortBy === 'name') {
+                        return a.name.localeCompare(b.name);
+                    } else if (sortBy === 'date_utc') {
+                        return new Date(a.date_utc) - new Date(b.date_utc);
+                    } else if (sortBy === 'success') {
+                        return a.success - b.success;
+                    }
+                });
+                launchList.innerHTML = '';
+                data.forEach(launch => {
+                    const li = document.createElement('li');
+                    li.innerHTML = `<strong>Misión:</strong> ${launch.name}<br>
+                                    <strong>Fecha de Lanzamiento:</strong> ${new Date(launch.date_utc).toDateString()}<br>
+                                    <strong>Éxito:</strong> ${launch.success ? 'Sí' : 'No'}`;
+                    launchList.appendChild(li);
+                });
+            })
+            .catch(error => console.error('Error al cargar la API de SpaceX:', error));
+    }
+    sortButton.addEventListener('click', () => {
+        const sortBy = sortSelect.value;
+        fetchDataAndDisplay(sortBy);
+    });
+    fetchDataAndDisplay('name');
+});
