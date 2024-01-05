47cc385a2947e7157853d15c1839180ce6185185
AlejandroP75 <diego_palacio5@hotmail.com>
2024-01-05 01:06:57 -0500

    lol

---
+++ Java/CalcularEdad/src/CalculadoraEdad.java
@@ -0,0 +1,40 @@
+public class CalculadoraEdad {
+    private int diaNacimiento;
+    private int mesNacimiento;
+    private int annoNacimiento;
+    
+    public CalculadoraEdad(int diaNacimiento, int mesNacimiento, int annoNacimiento) {
+        this.diaNacimiento = diaNacimiento;
+        this.mesNacimiento = mesNacimiento;
+        this.annoNacimiento = annoNacimiento;
+    }
+
+    public int getDiaNacimiento() {
+        return diaNacimiento;
+    }
+
+    public void setDiaNacimiento(int diaNacimiento) {
+        this.diaNacimiento = diaNacimiento;
+    }
+
+    public int getMesNacimiento() {
+        return mesNacimiento;
+    }
+
+    public void setMesNacimiento(int mesNacimiento) {
+        this.mesNacimiento = mesNacimiento;
+    }
+
+    public int getAnnoNacimiento() {
+        return annoNacimiento;
+    }
+
+    public void setAnnoNacimiento(int annoNacimiento) {
+        this.annoNacimiento = annoNacimiento;
+    }
+
+    public int calcularEdad(){
+        
+        return(0);
+    }
+}
