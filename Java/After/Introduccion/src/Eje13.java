40d38af7cc21c1bdd674e4f8f2c3cabd21636756
AlejandroP75 <diego_palacio5@hotmail.com>
2024-01-25 19:28:55 -0500

    comit

--- Java/After/Introduccion/src/Eje13.java
+++ Java/After/Introduccion/src/Eje13.java
@@ -10,7 +10,27 @@ public class Eje13 {
         System.out.print("Juegos ganados por B: ");
         int b = leer.nextInt();
 
-        
+        if(((a >= 7) && (b >= 7)) || ((a == 7) && (b < 5)) || ((b == 7) && (a < 5)) || (a < 0) || (b < 0)){
+            System.out.print("El partido es invalido");
+        }else{
+            if((a >= 5) && (b >= 5)){
+                if(a == 7){
+                    System.out.print("Gano A");
+                }else if(b == 7){
+                    System.out.print("Gano B");
+                }else{
+                    System.out.print("El partido continua");
+                }
+            }else{
+                if((a == 6) && (b >= 4)){
+                    System.out.print("Gano A");
+                }else if((b == 6) && (a >= 4)){
+                    System.out.print("Gano B");
+                }else{
+                    System.out.print("El partido continua");
+                }
+            }
+        }
 
         leer.close();
     }
